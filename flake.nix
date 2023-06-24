c45f204185da60ad0d1f2fd8fca275a2a1dddd7b
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-06-24 23:51:18 +0100

    Add a flake.nix/lock, deploy to cachix on main

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Run a process with a decorated stdout/stderr";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in
+      rec {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+            pname = "stdecor";
+            version = "0.1.2";
+            src = self;
+            cargoLock.lockFile = ./Cargo.lock;
+        };
+      }
+    );
+}
